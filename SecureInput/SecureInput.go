4782e56958c5fb89319e66099e9806237b62fc86
Vlad <gluk256@gmail.com>
2016-08-18 19:08:55 +0200

    simplified alphabet logic, added hashes

--- SecureInput/SecureInput.go
+++ SecureInput/SecureInput.go
@@ -1,19 +1,60 @@
 package SecureInput
 
 import (
+	crand "crypto/rand"
+	mrand "math/rand"
 	"fmt"
 	"time"
 	"os"
 	"os/exec"
-	crand "crypto/rand"
-	mrand "math/rand"
+	"crypto/sha256"
 )
 
-const alphabet string = "abcdefghijklmnopqrstuvwxyz 0123456789"
-const alphabetShift string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ )!@#$%^&*("
+// you can always arbitrary extend the alphabet (add capital letters, special characters, etc.)
+const alphabet string = "abcdefghijklmnopqrstuvwxyz 0123456789,."
 const sz = len(alphabet)
-const isCapsEnabled bool = false
-var rndAlphabet, rndAlphabetShift string
+var rndAlphabet string
+
+
+func printSpaced(s string) {
+	x := make([]byte, sz*2)
+	for i, c := range s {
+		x[i*2] = byte(c)
+		x[i*2 + 1] = ' '
+	}
+	fmt.Print(string(x))
+}
+
+func readSafeInput() string {
+	//fmt.Println("SecureInput version 24")
+	printSpaced(alphabet)
+	fmt.Println()
+	var s, res string
+	var b []byte = make([]byte, 1)
+	done := false
+
+	for !done {
+		randomizeAlphabet()
+		fmt.Print("\r")
+		printSpaced(rndAlphabet)
+		os.Stdin.Read(b)
+
+		switch b[0] {
+		case  96: // '~': do nothing (reshuffle)
+		case 126: // shift + '~': do nothing (reshuffle)
+		case 127: // backspace
+			if i := len(s); i > 0 {
+				s = s[:i-1]
+			}
+		default:
+			res, done = recoverRandomizedByte(b[0])
+			s += res
+		}
+	}
+
+	fmt.Println()
+	return s
+}
 
 func randNum() int {
 	sum := time.Now().Nanosecond()
@@ -29,85 +70,27 @@ func randNum() int {
 }
 
 func randomizeAlphabet() {
+	// shift
 	rnd := randNum()
-	if rnd >= sz {
-		panic("weird error in randomizeAlphabet()")
-	}
 	rndAlphabet = alphabet[rnd:] + alphabet[:rnd]
-	rndAlphabetShift = alphabetShift[rnd:] + alphabetShift[:rnd]
 
 	// shuffle
 	var x []byte = []byte(rndAlphabet)
-	var xshift []byte = []byte(rndAlphabetShift)
 	perm := mrand.Perm(sz)
 	for j, v := range perm {
 		x[j] = rndAlphabet[v]
-		xshift[j] = rndAlphabetShift[v]
 	}
 	rndAlphabet = string(x)
-	rndAlphabetShift = string(xshift)
 }
 
 func recoverRandomizedByte(c byte) (string, bool) {
-	if c >= 32 && c <= 122 {
-		for i := 0; i < sz; i++ {
-			if rndAlphabet[i] == c {
-				r := alphabet[i]
-				return string(r), false
-			}
-		}
-		if isCapsEnabled {
-			for i := 0; i < sz; i++ {
-				if rndAlphabetShift[i] == c {
-					r := alphabet[i]
-					if r >= 97 && r <= 122 {
-						r -= 32
-					}
-					return string(r), false
-				}
-			}
+	for i := 0; i < sz; i++ {
+		if rndAlphabet[i] == c {
+			r := alphabet[i]
+			return string(r), false
 		}
 	}
 	return "", true
-}
-
-func printSpaced(s string) {
-	x := make([]byte, sz*2)
-	for i, c := range s {
-		x[i*2] = byte(c)
-		x[i*2 + 1] = ' '
-	}
-	fmt.Print(string(x))
-}
-
-func readSafeInput() string {
-	//fmt.Println("SecureInput version 19")
-	printSpaced(alphabet)
-	fmt.Println()
-	var s, res string
-	var b []byte = make([]byte, 1)
-	done := false
-	for !done {
-		randomizeAlphabet()
-		fmt.Print("\r")
-		printSpaced(rndAlphabet)
-
-		os.Stdin.Read(b)
-
-		switch b[0] {
-		case  96: // '~': reshuffle
-		case 126: // shift + '~': reshuffle
-		case 127: // backspace
-			if i := len(s); i > 0 {
-				s = s[:i-1]
-			}
-		default:
-			res, done = recoverRandomizedByte(b[0])
-			s += res
-		}
-	}
-	fmt.Println()
-	return s
 }
 
 func ReadFromTerminal() string {
@@ -117,15 +100,33 @@ func ReadFromTerminal() string {
 	return readSafeInput()
 }
 
-func ReadFromTerminalTst() string {
-	exec.Command("stty", "-F", "/dev/tty", "cbreak", "min", "1").Run()
-	exec.Command("stty", "-F", "/dev/tty", "-echo").Run()
-	defer exec.Command("stty", "-F", "/dev/tty", "echo").Run()
-	var b []byte = make([]byte, 1)
-	for {
-		os.Stdin.Read(b)
-		fmt.Println("I got the byte", b, "(" + string(b) + ")")
-	}
-	return "test complete"
+//func ReadFromTerminalTst() string {
+//	exec.Command("stty", "-F", "/dev/tty", "cbreak", "min", "1").Run()
+//	exec.Command("stty", "-F", "/dev/tty", "-echo").Run()
+//	defer exec.Command("stty", "-F", "/dev/tty", "echo").Run()
+//	var b []byte = make([]byte, 1)
+//	for {
+//		os.Stdin.Read(b)
+//		fmt.Println("I got the byte", b, "(" + string(b) + ")")
+//	}
+//	return "test finished"
+//}
+
+/////////////////////////////////////////////////////////////////////////////////
+
+func hash(s string) []byte {
+	h := sha256.New()
+	h.Write([]byte(s))
+	return h.Sum(nil)
 }
 
+func SHash(s string) string {
+	h := hash(s)
+	x := fmt.Sprintf("%x", h)
+	return x
+}
+
+func XHash() string {
+	s := ReadFromTerminal()
+	return SHash(s)
+}
--- xmd/main.go
+++ xmd/main.go
@@ -3,10 +3,23 @@ package main
 import (
 	"fmt"
 	"github.com/gluk256/crypto/SecureInput"
+	"os"
 )
 
+
 func main() {
-	s := SecureInput.ReadFromTerminal()
+	var x string
+	if len(os.Args) > 1 {
+		x = os.Args[1]
+		if '-' == x[0] {
+			x = SecureInput.XHash()
+		} else {
+			x = SecureInput.SHash(x)
+		}
+	} else {
+		x = SecureInput.ReadFromTerminal()
+	}
+
 	fmt.Println()
-	fmt.Println(s)
+	fmt.Println(x)
 }
