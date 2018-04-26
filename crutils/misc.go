3c0a25032da66f2b3c4705f96799d95bb2a79465
Vlad <gluk256@gmail.com>
2018-04-26 10:49:22 +0200

    func sha2s() added

--- crutils/misc.go
+++ crutils/misc.go
@@ -1,9 +1,18 @@
 package crutils
 
-import "crypto/sha256"
+import (
+	"crypto/sha256"
+	"fmt"
+)
 
 func Sha2(s []byte) []byte {
 	h := sha256.New()
 	h.Write(s)
 	return h.Sum(nil)
+}
+
+func Sha2s(s []byte) string {
+	h := Sha2(s)
+	x := fmt.Sprintf("%x", h)
+	return x
 }
