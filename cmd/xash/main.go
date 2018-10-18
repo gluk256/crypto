626dbb4f1664f2d71f775ed72b49a39748c5f26d
Vlad <gluk256@gmail.com>
2018-10-18 12:25:02 +0400

    file hash added

--- cmd/xash/main.go
+++ cmd/xash/main.go
@@ -3,10 +3,20 @@ package main
 import (
 	"fmt"
 	"os"
+	"io/ioutil"
 
 	"github.com/gluk256/crypto/terminal"
 	"github.com/gluk256/crypto/crutils"
 )
+
+func readFile(name string) []byte {
+	b, err := ioutil.ReadFile(name)
+	if err != nil {
+		fmt.Printf("Error: can not read file [%s]\n", name)
+		os.Exit(0)
+	}
+	return b
+}
 
 func main() {
 	var txt []byte
@@ -17,6 +27,7 @@ func main() {
 	} else {
 		txt = terminal.PasswordModeInput()
 	}
+	//txt = readFile(string(txt))
 	hash := crutils.Sha2(txt)
 	fmt.Printf("%x\n", hash)
 }
