c0d9bc6244d8a870228c53d07e202766a371ccc3
dex <gluk256@gmail.com>
2019-12-28 14:21:26 +0200

    asym library complete

--- algo/asym/asym.go
+++ algo/asym/asym.go
@@ -4,16 +4,19 @@ import (
 	"crypto/ecdsa"
 	"crypto/rand"
 	"errors"
+	"fmt"
 
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/crypto/ecies"
 	"github.com/gluk256/crypto/algo/keccak"
 )
 
-// I am unable to implement elliptic curve cryptography, and therefore must rely on external libraries.
-// These functions are imported from ethereum project, which is one of the most audited open source projects.
+// I am unable to implement elliptic curve cryptography, and have to rely on external libraries.
+// These functions are imported from ethereum - one of the most audited open source projects.
 
-func Key(raw []byte) (key *ecdsa.PrivateKey, err error) {
+const EncryptedSizeDiff = 113
+
+func ImportPrivateKey(raw []byte) (key *ecdsa.PrivateKey, err error) {
 	return crypto.ToECDSA(raw)
 }
 
@@ -21,9 +24,35 @@ func GenerateKey() (*ecdsa.PrivateKey, error) {
 	return crypto.GenerateKey()
 }
 
+func ExportPubKey(key *ecdsa.PublicKey) (res []byte, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Warning: ExportPubKey failed: ", r)
+			err = errors.New("ExportPubKey failed")
+		}
+	}()
+
+	res = crypto.CompressPubkey(key)
+	return res, err
+}
+
+func ImportPubKey(data []byte) (res *ecdsa.PublicKey, err error) {
+	return crypto.DecompressPubkey(data)
+}
+
 func Sign(key *ecdsa.PrivateKey, data []byte) (signature []byte, err error) {
 	hash := keccak.Digest(data, 32)
 	return crypto.Sign(hash, key)
+}
+
+func SigToPub(data []byte, sig []byte) (res []byte, err error) {
+	var pub *ecdsa.PublicKey
+	hash := keccak.Digest(data, 32)
+	pub, err = crypto.SigToPub(hash, sig)
+	if err == nil {
+		res, err = ExportPubKey(pub)
+	}
+	return res, err
 }
 
 func Encrypt(key *ecdsa.PublicKey, data []byte) (res []byte, err error) {
@@ -32,15 +61,4 @@ func Encrypt(key *ecdsa.PublicKey, data []byte) (res []byte, err error) {
 
 func Decrypt(key *ecdsa.PrivateKey, data []byte) (res []byte, err error) {
 	return ecies.ImportECDSA(key).Decrypt(data, nil, nil)
-}
-
-func SigToPub(data []byte, sig []byte) (pub *ecdsa.PublicKey, err error) {
-	defer func() {
-		pub = nil
-		err = errors.New("invalid signature")
-		recover()
-	}()
-
-	hash := keccak.Digest(data, 32)
-	return crypto.SigToPub(hash, sig)
 }
---
+++ algo/asym/asym_test.go
@@ -0,0 +1,185 @@
+package asym
+
+import (
+	"bytes"
+	"crypto/ecdsa"
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/gluk256/crypto/algo/keccak"
+	"github.com/gluk256/crypto/algo/primitives"
+)
+
+func TestGeneral(t *testing.T) {
+	seed := time.Now().Unix()
+	rand.Seed(seed)
+
+	key, err := GenerateKey()
+	if err != nil {
+		t.Fatalf("key generation failed: %s", err.Error())
+	}
+
+	if key.Params().BitSize != 256 {
+		t.Fatalf("key size = %d", key.Params().BitSize)
+	}
+
+	singleKeyTest(t, key)
+
+	data := generateRandomBytes(t)
+	hash := keccak.Digest(data, 32)
+	key, err = ImportPrivateKey(hash)
+	if err != nil {
+		t.Fatalf("key import failed: %s", err.Error())
+	}
+
+	singleKeyTest(t, key)
+
+	k2, err := ImportPrivateKey(hash)
+	if err != nil {
+		t.Fatalf("key import failed: %s", err.Error())
+	}
+
+	p1, err := ExportPubKey(&key.PublicKey)
+	if err != nil {
+		t.Fatalf("key export failed: %s", err.Error())
+	}
+
+	p2, err := ExportPubKey(&k2.PublicKey)
+	if err != nil {
+		t.Fatalf("key export failed: %s", err.Error())
+	}
+
+	if !bytes.Equal(p1, p2) {
+		t.Fatal("consistency check failed")
+	}
+}
+
+func singleKeyTest(t *testing.T, key *ecdsa.PrivateKey) {
+	raw, err := ExportPubKey(&key.PublicKey)
+	if err != nil {
+		t.Fatalf("key export failed: %s", err.Error())
+	}
+
+	pub, err := ImportPubKey(raw)
+	if err != nil {
+		t.Fatalf("re-import failed: %s", err.Error())
+	}
+
+	raw2, err := ExportPubKey(pub)
+	if err != nil {
+		t.Fatalf("re-export failed: %s", err.Error())
+	}
+
+	if !bytes.Equal(raw, raw2) {
+		t.Fatal("re-export produced wrong key")
+	}
+
+	var data, sig []byte
+	for i := 0; i < 8; i++ {
+		data = generateRandomBytes(t)
+		sig, err = Sign(key, data)
+		if err != nil {
+			t.Fatalf("sign failed: %s", err.Error())
+		}
+
+		recovered, err := SigToPub(data, sig)
+		if err != nil {
+			t.Fatalf("signature recovery failed: %s", err.Error())
+		}
+
+		if !bytes.Equal(raw, recovered) {
+			t.Fatalf("signature recovery error\n%x vs. %x", raw, recovered)
+		}
+
+		j := rand.Int() % len(sig)
+		sig[j]++
+		r, err := SigToPub(data, sig)
+		if err == nil {
+			if bytes.Equal(r, raw) {
+				t.Fatal("re-export false positive")
+			}
+		}
+	}
+
+	raw2[0]++
+	pub, err = ImportPubKey(raw2)
+	if err == nil {
+		raw2, err = ExportPubKey(pub)
+		if err != nil {
+			t.Fatalf("second re-export failed: %s", err.Error())
+		}
+
+		if bytes.Equal(raw, raw2) {
+			t.Fatal("re-export false positive")
+		}
+	}
+}
+
+func TestEncryption(t *testing.T) {
+	seed := time.Now().Unix()
+	rand.Seed(seed)
+
+	for i := 0; i < 8; i++ {
+		key, err := GenerateKey()
+		if err != nil {
+			t.Fatalf("key generation failed: %s", err.Error())
+		}
+
+		orig := generateRandomBytes(t)
+		x := make([]byte, len(orig))
+		copy(x, orig)
+
+		encrypted, err := Encrypt(&key.PublicKey, orig)
+		if err != nil {
+			t.Fatalf("encryption failed: %s", err.Error())
+		}
+
+		if !bytes.Equal(x, orig) {
+			t.Fatal("orig destroyed")
+		}
+
+		ok := primitives.IsDeepNotEqual(orig, encrypted, len(orig))
+		if !ok {
+			t.Fatalf("failed encrypt deep check, round %d with seed %d", i, seed)
+		}
+
+		diff := len(encrypted) - len(orig)
+		if diff != EncryptedSizeDiff {
+			t.Fatalf("wrong encrypted size diff [%d vs. %d]", diff, EncryptedSizeDiff)
+		}
+
+		y := make([]byte, len(encrypted))
+		copy(y, encrypted)
+
+		decrypted, err := Decrypt(key, encrypted)
+		if err != nil {
+			t.Fatalf("decryption failed: %s", err.Error())
+		}
+
+		if !bytes.Equal(decrypted, orig) {
+			t.Fatalf("decryption bug, round %d with seed %d", i, seed)
+		}
+
+		if !bytes.Equal(y, encrypted) {
+			t.Fatal("src destroyed")
+		}
+
+		j := rand.Int() % len(encrypted)
+		encrypted[j]++
+		decrypted, err = Decrypt(key, encrypted)
+		if err == nil {
+			t.Fatalf("decryption false positive: %s", err.Error())
+		}
+	}
+}
+
+func generateRandomBytes(t *testing.T) []byte {
+	sz := rand.Intn(256) + 256
+	b := make([]byte, sz)
+	_, err := rand.Read(b)
+	if err != nil {
+		t.Fatal("failed to generate random bytes")
+	}
+	return b
+}
